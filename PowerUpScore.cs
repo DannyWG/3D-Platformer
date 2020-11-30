42f126662967a9921c3d8115fbc55bf446928c46
DannyWG <75269762+DannyWG@users.noreply.github.com>
2020-11-30 18:11:17 +0000

    Add files via upload

---
+++ PowerUpScore.cs
@@ -0,0 +1,29 @@
+﻿// Danny Gnaulati point tracker script
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class PowerUpScore : MonoBehaviour
+{
+    public static PowerUpScore instance;
+    int score;
+    public TextMeshProUGUI text;
+    // making sure the score starts at zero
+    private void Start()
+    {
+        if(instance == null)
+        {
+            instance = this;
+        }
+
+    }
+    // When the player collects the collectable the score will increase
+    public void ChangeScore(int CoinValue)
+    {
+        score += CoinValue;
+        text.text = "x" + score.ToString();
+    }
+
+
+}
