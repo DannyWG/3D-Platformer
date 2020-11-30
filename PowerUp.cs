280d6999a4422a61dd46ce50823a449dad955e30
DannyWG <75269762+DannyWG@users.noreply.github.com>
2020-11-30 18:10:56 +0000

    Add files via upload

---
+++ PowerUp.cs
@@ -0,0 +1,29 @@
+﻿// Danny Gnaulati Collecting power up script
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PowerUp : MonoBehaviour
+{
+
+    // Declaring that the game object is a collectable
+    // making the value of each collectable 1
+    public GameObject collectable;
+    public int coinValue = 1;
+
+    // If the player comes into contact with this objects collider it will be destroyed
+    // maiking the score chnage when the player hits the collectables collider
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "Player")
+        {
+            PowerUpScore.instance.ChangeScore(coinValue);
+            Debug.Log("I have hit" + other.tag);
+            Destroy(collectable);
+        }
+
+    }
+
+
+}
+
